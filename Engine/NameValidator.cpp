bcf2bcb4c95200bebb5b197da19173fd8c064dbc
Dr_Pepper <d0kt0rp3pp3r@gmail.com>
2018-04-04 22:56:58 +0200

    Filter out  C/C++ Keywords in MakeValidName

--- Engine/NameValidator.cpp
+++ Engine/NameValidator.cpp
@@ -1,34 +1,36 @@
 #include "NameValidator.hpp"
 
 #include <cctype>
+#include <unordered_set>
 
 #include "ObjectsStore.hpp"
 
 std::string MakeValidName(std::string&& name)
 {
+	static std::unordered_set<char> disallowedChars = {
+		' ', '?', '+', '-', ':', '/', '^', '(', ')', '[',
+		']', '<', '>', '&', '.', '#', '\'', '"', '%',
+	};
+
+	static std::unordered_set<std::string> disallowedWords = {
+		"alignas", "alignof", "and", "and_eq", "asm", "auto", "bitand", "bitor",
+		"bool", "break", "case", "catch", "char", "char16_t", "char32_t", "class",
+		"compl", "const", "constexpr", "const_cast", "continue", "decltype", "default",
+		"delete", "do", "double", "dynamic_cast", "else", "enum", "explicit", "export",
+		"extern", "false", "float", "for", "friend", "goto", "if", "inline", "int",
+		"long", "mutable", "namespace", "new", "noexcept", "not", "not_eq", "nullptr",
+		"operator", "or", "or_eq", "private", "protected", "public", "register",
+		"reinterpret_cast", "return", "short", "signed", "sizeof", "static", "static_assert",
+		"static_cast", "struct", "switch", "template", "this", "thread_local", "throw",
+		"true", "try", "typedef", "typeid", "typename", "union", "unsigned", "using",
+		"virtual", "void", "volatile", "wchar_t", "while", "xor", "xor_eq"
+	};
+
 	std::string valid(name);
 
 	for (auto i = 0u; i < name.length(); ++i)
 	{
-		if (valid[i] == ' '
-			|| valid[i] == '?'
-			|| valid[i] == '+'
-			|| valid[i] == '-'
-			|| valid[i] == ':'
-			|| valid[i] == '/'
-			|| valid[i] == '^'
-			|| valid[i] == '('
-			|| valid[i] == ')'
-			|| valid[i] == '['
-			|| valid[i] == ']'
-			|| valid[i] == '<'
-			|| valid[i] == '>'
-			|| valid[i] == '&'
-			|| valid[i] == '.'
-			|| valid[i] == '#'
-			|| valid[i] == '\''
-			|| valid[i] == '"'
-			|| valid[i] == '%')
+		if (disallowedChars.find(valid[i]) != disallowedChars.end())
 		{
 			valid[i] = '_';
 		}
@@ -36,7 +38,7 @@ std::string MakeValidName(std::string&& name)
 
 	if (!valid.empty())
 	{
-		if (std::isdigit(valid[0]))
+		if (std::isdigit(valid[0]) || disallowedWords.find(valid) != disallowedWords.end())
 		{
 			valid = '_' + valid;
 		}
